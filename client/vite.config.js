02ba037b0546a366c1f725c19e7220f0973419b2
ivanhez <64239629+ivanhez@users.noreply.github.com>
2025-01-08 16:07:09 -0600

    keycsr

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from 'fs'
+
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // or true
+    port: 443,
+    https: {
+      key: fs.readFileSync('private.key'),
+      cert: fs.readFileSync('csr.pem'),
+    },
+  },
   plugins: [react()],
-})
+});
